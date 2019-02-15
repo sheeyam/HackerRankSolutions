62057a8098db479cd4215433c08bdb454d85e6df
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-02-14 21:21:20 -0600

    Create MaxArraySum.java

---
+++ MaxArraySum.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maxSubsetSum function below.
+    static int maxSubsetSum(int[] arr) {
+        int finalValue = 0;
+        int useValue = 0;
+        int notUseValue = 0;
+
+        for(int i=0; i < arr.length; i++) {
+
+            finalValue = Math.max(finalValue, notUseValue + arr[i]);
+
+            int nextUseValue = notUseValue + arr[i];
+            int nextNotUseValue = Math.max(useValue, notUseValue); 
+
+            useValue = nextUseValue;
+            notUseValue = nextNotUseValue;
+
+        }
+        return finalValue;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = maxSubsetSum(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
